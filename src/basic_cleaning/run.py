43215d96d33482ca44084610472354958b3840d8
lawrenceko <lawko698@gmail.com>
2023-01-29 01:08:33 +0800

    add geolocation filter

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -41,6 +41,9 @@ def go(args):
     df = df[idx].copy()
     # Convert last_review to datetime
     df['last_review'] = pd.to_datetime(df['last_review'])
+    # Check geolocation
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
 
     logger.info("Creating artifact")
     df.to_csv("clean_sample.csv", index=False)
